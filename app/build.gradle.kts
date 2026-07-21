a8d75a5fe00e0163cb1329fab5d1a39275cc76a3
Ubuntu <ubuntu@localhost.localdomain>
2026-07-21 19:27:00 +0800

    Fix Android build issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,8 @@ android {
     packaging {
         jniLibs {
             excludes.add("META-INF/*")
+            pickFirsts.add("lib/arm64-v8a/libbass.so")
+            pickFirsts.add("lib/arm64-v8a/libbass_ssl.so")
         }
         resources {
             excludes.add("META-INF/*")
