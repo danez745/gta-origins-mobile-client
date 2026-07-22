59c2b56aef3d5052281119d94779916c946382bf
Ubuntu <ubuntu@localhost.localdomain>
2026-07-22 16:21:50 +0800

    fix: allow emulator startup without imm library

--- android/app/src/main/java/com/touch/mobile/dark/MainGTA.java
+++ android/app/src/main/java/com/touch/mobile/dark/MainGTA.java
@@ -20,7 +20,11 @@ public class MainGTA extends WarMedia {
         try {
             vmVersion = System.getProperty("java.vm.version");
             System.out.println("vmVersion " + vmVersion);
-            System.loadLibrary("ImmEmulatorJ");
+            if (BuildConfig.LOAD_IMM_EMULATOR) {
+                System.loadLibrary("ImmEmulatorJ");
+            } else {
+                System.out.println("Skipping ImmEmulatorJ load");
+            }
         } catch (ExceptionInInitializerError | UnsatisfiedLinkError ignored) {
         }
 
